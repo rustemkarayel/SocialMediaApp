979ae2e3c69b3711a0b12347ac4f0fa0e0c1c2d9
Yusufhan26 <114617848+Yusufhan26@users.noreply.github.com>
2022-12-03 23:00:50 +0300

    CommentLike class'ı ve varlıkları oluşturuldu.

---
+++ EntityLayer/CommentLike.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class CommentLike
+    {
+        [Key]
+        public int CommentLikeId { get; set; }
+        [StringLength(5)]
+        public string CommentLikeTime { get; set; }
+    }
+}
