98ee430a817553ec6c61440514776bcf529aa3eb
Yusufhan26 <114617848+Yusufhan26@users.noreply.github.com>
2022-12-03 22:51:21 +0300

    Post class'ı ve varlıkları oluşturuldu.

---
+++ EntityLayer/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        [StringLength(10)]
+        public string GenerateDate { get; set; }
+        public int Content { get; set; }
+        [StringLength(300)]
+        public string Description { get; set; }
+    }
+}
