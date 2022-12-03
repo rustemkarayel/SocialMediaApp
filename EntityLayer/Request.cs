f8c99e9919801297fbb5c7ed713211a488a08123
Yusufhan26 <114617848+Yusufhan26@users.noreply.github.com>
2022-12-03 22:41:08 +0300

    Request class'ın varlıklarını oluşturduk.

--- EntityLayer/Request.cs
+++ EntityLayer/Request.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,11 @@ namespace EntityLayer
 {
     public class Request
     {
+        [Key]
+        public int RequestId { get; set; }
+        [StringLength(5)]
+        public string RequestTime { get; set; }
+        public bool RequestState { get; set; }
+        //Users ile ilişkilendirilecek.
     }
 }
