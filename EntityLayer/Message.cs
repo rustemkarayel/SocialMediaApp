074d62bf28cfad292d75074d7e84a97905b609b1
Rüstem Karayel <rustemkarayel07@gmail.com>
2022-12-03 23:22:14 +0300

    Message varlığı oluşturuldu.

--- EntityLayer/Message.cs
+++ EntityLayer/Message.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,15 @@ namespace EntityLayer
 {
     public class Message
     {
+        [Key]
+        public int MessageId { get; set; }
+        public string Content { get; set; }
+        public DateOnly SendDate { get; set; }
+        public TimeOnly SendTime { get; set; }
+
+        //User ile ilişkilendirilecek.
+        [ForeignKey ("Sender")]
+        public int SenderId { get; set; }
+        public User Sender { get; set; }
     }
 }
