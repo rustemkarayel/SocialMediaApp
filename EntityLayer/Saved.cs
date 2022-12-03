c5978389a495ffc4bdc070d6dcb02f0a55b3bc45
Rüstem Karayel <rustemkarayel07@gmail.com>
2022-12-04 00:43:05 +0300

    Saved varlığı oluşturuldu.

---
+++ EntityLayer/Saved.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class Saved
+    {
+        [Key]
+        public int SavedId { get; set; }
+
+        //User ile ilişkilendirilecek.
+        public int UserId { get; set; }
+        public User user { get; set; }
+
+        //Post ile ilişkilendirilecek.
+        public int PostId { get; set; }
+        public Post post { get; set; }
+
+        //SavedCollection ile ilişkilendirilecek.
+        public ICollection<SavedCollection> SavedCollections { get; set; }
+
+
+    }
+}
