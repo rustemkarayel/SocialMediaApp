167a9fd52d07cfbaaaa88392b28d7b8ead289d24
Yusufhan26 <114617848+Yusufhan26@users.noreply.github.com>
2022-12-03 22:56:22 +0300

    PostLike class'ı ve varlıkları oluşturuldu.

---
+++ EntityLayer/PostLike.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class PostLike
+    {
+        [Key]
+        public int PostLikeId { get; set; }
+        [StringLength(5)]
+        public string LikeTime { get; set; }
+    }
+}
