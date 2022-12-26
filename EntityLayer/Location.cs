5f0848b1def9b71266172d46a3b6852406aa23b3
MHusna <mehmethusnak@gmail.com>
2022-12-27 00:34:59 +0300

    Location varlığı oluşturuldu.

---
+++ EntityLayer/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class Location
+    {
+        [Key]
+        public int LocationId { get; set; }
+        public string LocationName { get; set; }
+
+        //Post ile ilişkilendirilecek.
+        public ICollection<Post> Posts { get; set; }
+    }
+}
