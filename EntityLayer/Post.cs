0dd0cc149e7873bdb0b743498c4b63d5c8c5ac5c
Yusufhan-Erbas <63731504+Yusufhan-Erbas@users.noreply.github.com>
2022-12-27 21:43:59 +0300

    İlişkilendirmeler yapıldı.

--- EntityLayer/Post.cs
+++ EntityLayer/Post.cs
@@ -13,12 +13,31 @@ namespace EntityLayer
         [Key]
         public int PostId { get; set; }        
         public DateTime GenerateDate { get; set; }
-        public int Content { get; set; }
+        [StringLength(300)]
+        public string Content { get; set; }
         [StringLength(300)]
         public string Description { get; set; }
-        //User ile ilişkilendirilecek.
-        [ForeignKey("Creator")]
+        //User ile ilişkilendirilecek.       
         public int CreatorId { get; set; }
         public User Creator { get; set; }
+        //Location ile ilişkilendirilecek.
+        public int LocationId {get; set;}
+        public Location Location {get; set;}
+        //Type ile ilişkilendirilecek.
+        public int TypeId {get; set;}
+        public Type Type {get; set;}
+        //Comment ile ilişkilendirilecek.
+        public int CommentId {get; set;}
+        public Comment Comment {get; set;}
+        //Tag ile ilişkilendirilecek.
+        public int TagId {get; set;}
+        public Tag Tag {get; set;}
+        //Postlike ile ilişkilendirilecek.
+        public int PostLikeId {get; set;}
+        public PostLike PostLike {get; set;}
+        //Saved ile ilişkilendirilecek.
+        public int SavedId {get; set;}
+        public Saved Saved {get; set;}
+        
     }
 }
