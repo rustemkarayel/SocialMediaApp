f45f12df3a35be9f4f6ccb19613d1e0fb1366844
rustemkarayel <rustemkarayel07@gmail.com>
2022-12-28 11:56:44 +0300

    Database bağlantı metodu ve tablo isimleri verildi.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,31 @@
+﻿using EntityLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("connection string yaz");
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<SavedCollection> SavedCollections { get; set; }
+        public DbSet<Saved> Saveds { get; set; }
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<PostLike> PostLikes { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<CommentLike> CommentLikes { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Collection> Collections { get; set; }
+    }
+}
