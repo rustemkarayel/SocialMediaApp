2dfe90543e069a1e4a010d01f31f0b340c5c69d5
rustemkarayel <rustemkarayel07@gmail.com>
2023-01-02 22:21:36 +0300

    User için Service ve Manager oluşturuldu.

---
+++ BusinessLayer/Abstract/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IUserService
+    {
+        void userInsert(User user);
+        void userDelete(User user);
+        void userUpdate(User user);
+        List<User> userList();
+        User userGetById(int id);
+        User userGetByName(string name);
+    }
+}
