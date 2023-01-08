c89ead92f18f231ef930c9bf37b9fbd8ec5218e4
rustemkarayel <rustemkarayel07@gmail.com>
2023-01-08 16:32:31 +0300

    Saved için MVC oluşturuldu.

---
+++ SocialMediaApp/Controllers/SavedController.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer;
+using Microsoft.AspNetCore.Mvc;
+using SocialMediaApp.Models;
+
+namespace SocialMediaApp.Controllers
+{
+    public class SavedController : Controller
+    {
+        SavedManager sm=new SavedManager(new EfSavedRepository());
+        UserManager um=new UserManager(new EfUserRepository());
+        PostManager pm=new PostManager(new EfPostRepository());
+        public IActionResult Index()
+        {
+            var saveds=sm.SavedList();
+            return View(saveds);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            SavedUserPostModel supm=new SavedUserPostModel();
+            supm.SavedModel = new Saved();
+            supm.UserModel = um.UserList();
+            supm.PostModel = pm.PostList();
+            return View(supm);
+        }
+     
+
+    }
+}
---
+++ SocialMediaApp/Models/SavedUserPostModel.cs
@@ -0,0 +1,11 @@
+﻿using EntityLayer;
+
+namespace SocialMediaApp.Models
+{
+    public class SavedUserPostModel
+    {
+        public Saved SavedModel { get; set; }
+        public IEnumerable<User> UserModel { get; set; }
+        public IEnumerable<Post> PostModel { get; set; }
+    }
+}
