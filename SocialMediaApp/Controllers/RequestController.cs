b4bb1cdc5393b1f69c9aa6a28e98db240b0ce285
Yusufhan-Erbas <erbasyusufhan15@gmail.com>
2023-01-09 06:46:09 +0300

    Request conroller oluşturuldu ve view eklendi.

---
+++ SocialMediaApp/Controllers/RequestController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMediaApp.Controllers
+{
+    public class RequestController : Controller
+    {
+        RequestManager rm = new RequestManager(new EfRequestRepository());
+        public IActionResult Index()
+        {
+            var requests = rm.RequestList();
+            return View(requests);
+        }
+    }
+}
