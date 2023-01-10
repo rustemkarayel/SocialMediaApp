a79f5ba4b66c87fee5244a07ede4e0f09a8982fd
MHusna <mehmethusnak@gmail.com>
2023-01-10 23:13:17 +0300

    Location controller dosyası yazıldı.

---
+++ SocialMediaApp/Controllers/LocationController.cs
@@ -0,0 +1,94 @@
+﻿using Azure;
+using BusinessLayer.Concrete;
+using BusinessLayer.Validations;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SocialMediaApp.Models;
+
+namespace SocialMediaApp.Controllers
+{
+    public class LocationController : Controller
+    {
+        LocationManager locationManager = new LocationManager(new EfLocationRepository());
+        public IActionResult Index()
+        {
+            var locations = locationManager.LocationList();
+            return View(locations);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Location location)
+        {
+            LocationValidator locationValidator = new LocationValidator();
+            var result = locationValidator.Validate(location);
+
+            if (result.IsValid)
+            {
+                locationManager.LocationInsert(location);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return RedirectToAction("Add");
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Location location = locationManager.GetLocationById(id);
+            location.IsActive = false;
+            locationManager.LocationUpdate(location);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            Location location = locationManager.GetLocationById(id);
+
+            LocationLocationListModel locationLocationListModel = new LocationLocationListModel();
+            locationLocationListModel.locationModel = location;
+            locationLocationListModel.locationsModel = locationManager.LocationList();
+
+            return View(locationLocationListModel);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Location location)
+        {
+            LocationValidator locationValidator = new LocationValidator();
+            var result = locationValidator.Validate(location);
+            if (result.IsValid)
+            {
+                locationManager.LocationUpdate(location);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                LocationLocationListModel locationLocationListModel = new LocationLocationListModel();
+                locationLocationListModel.locationModel = location;
+                locationLocationListModel.locationsModel = locationManager.LocationList();
+
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+
+                return View(locationLocationListModel);
+            }
+        }
+    }
+}
