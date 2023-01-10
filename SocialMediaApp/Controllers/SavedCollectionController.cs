6092ba2b77623343f7dbce860c0493aafc273c4f
rustemkarayel <rustemkarayel07@gmail.com>
2023-01-10 22:48:01 +0300

    SavedCollection için MVC yazıldı.

---
+++ SocialMediaApp/Controllers/SavedCollectionController.cs
@@ -0,0 +1,66 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.Validations;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore;
+using SocialMediaApp.Models;
+using System.ComponentModel;
+
+namespace SocialMediaApp.Controllers
+{
+    public class SavedCollectionController : Controller
+    {
+        SavedCollectionManager scm = new SavedCollectionManager(new EfSavedCollectionRepository());
+        SavedManager sm = new SavedManager(new EfSavedRepository());
+        CollectionManager cm = new CollectionManager(new EfCollectionRepository());
+
+        public IActionResult Index()
+        {
+            var savedCollections = scm.SavedCollectionList();
+            return View(savedCollections);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            SavedCollectionSavedCollectionModel scscm = new SavedCollectionSavedCollectionModel();
+            scscm.SavedCollectionModel = new SavedCollection();
+            scscm.SavedModel = sm.SavedList();
+            scscm.CollectionModel = cm.CollectionList();
+            return View(scscm);
+        }
+        [HttpPost]
+        public IActionResult Add(SavedCollection savedCollection)
+        {
+            SavedCollectionValidator savedCollectionValidator = new SavedCollectionValidator();
+            var result = savedCollectionValidator.Validate(savedCollection);
+            if (result.IsValid)
+            {
+                scm.SavedCollectionInsert(savedCollection);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                SavedCollectionSavedCollectionModel scscm = new SavedCollectionSavedCollectionModel();
+                scscm.SavedCollectionModel = new SavedCollection();
+                scscm.SavedModel = sm.SavedList();
+                scscm.CollectionModel = cm.CollectionList();
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View(scscm);
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            SavedCollection savedCollection = scm.SavedCollectionGetById(id);
+            savedCollection.IsActive = false;
+            scm.SavedCollectionUpdate(savedCollection);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ SocialMediaApp/Models/SavedCollectionSavedCollectionModel.cs
@@ -0,0 +1,11 @@
+﻿using EntityLayer;
+
+namespace SocialMediaApp.Models
+{
+    public class SavedCollectionSavedCollectionModel
+    {
+        public SavedCollection SavedCollectionModel { get; set; }
+        public IEnumerable<Saved> SavedModel { get; set; }
+        public IEnumerable<Collection> CollectionModel { get; set; }
+    }
+}
