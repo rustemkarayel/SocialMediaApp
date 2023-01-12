5c643a051b15b01e4c11a2e7fb9ce3fd99c3f763
MHusna <mehmethusnak@gmail.com>
2023-01-12 20:26:30 +0300

    Collection validator oluşturuldu.

---
+++ BusinessLayer/Validations/CollectionValidator.cs
@@ -0,0 +1,15 @@
+﻿using EntityLayer;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Validations
+{
+    public class CollectionValidator:AbstractValidator<Collection>
+    {
+    }
+}
