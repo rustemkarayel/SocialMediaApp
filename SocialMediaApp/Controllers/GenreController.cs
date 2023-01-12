cca1de8271e4f1c4abaf53fdbfe5474456f6a74d
mhusna <mehmethusnak@gmail.com>
2023-01-12 13:33:11 +0300

    Genre controller oluşturuldu.

---
+++ SocialMediaApp/Controllers/GenreController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMediaApp.Controllers
+{
+    public class GenreController : Controller
+    {
+        GenreManager genreManager = new GenreManager(new EfGenreRepository());
+
+        public IActionResult Index()
+        {
+            return View(genreManager.GenreList());
+        }
+        
+        public IActionResult Delete(int id)
+        {
+            var genre = genreManager.GetGenreById(id);
+            genre.IsActive = false;
+            genreManager.GenreUpdate(genre);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
