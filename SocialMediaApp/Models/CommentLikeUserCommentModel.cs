556013573ae80b3ada3faaf373fee6da5b2027e8
Yusufhan-Erbas <erbasyusufhan15@gmail.com>
2023-01-12 13:27:11 +0300

    Request Add hatası çözüldü.

---
+++ SocialMediaApp/Models/CommentLikeUserCommentModel.cs
@@ -0,0 +1,11 @@
+﻿using EntityLayer;
+
+namespace SocialMediaApp.Models
+{
+    public class CommentLikeUserCommentModel
+    {
+        public CommentLike CommentLikeModel { get; set; }  
+        public IEnumerable<Comment> CommentModel { get; set; }
+        public IEnumerable<User> UserModel { get; set; }
+    }
+}
