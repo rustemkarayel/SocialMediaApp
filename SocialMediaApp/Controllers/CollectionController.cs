51ef738fea7efe0aa2cd00ed1bba1f52657711a7
mhusna <mehmethusnak@gmail.com>
2023-01-12 14:45:48 +0300

    Index sayfası için controller yazıldı.

---
+++ SocialMediaApp/Controllers/CollectionController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMediaApp.Controllers
+{
+    public class CollectionController : Controller
+    {
+        CollectionManager collectionManager = new CollectionManager(new EfCollectionRepository());
+
+        public IActionResult Index()
+        {
+            return View(collectionManager.CollectionList());
+        }
+    }
+}
