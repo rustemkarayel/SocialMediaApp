accbd5a1b9426ca40497df9b6ab47a8460b243ce
Yusufhan-Erbas <erbasyusufhan15@gmail.com>
2023-01-12 04:57:31 +0300

    PostLikeModel oluşturuldu.

---
+++ SocialMediaApp/Models/PostLikeUserPostModel.cs
@@ -0,0 +1,11 @@
+﻿using EntityLayer;
+
+namespace SocialMediaApp.Models
+{
+    public class PostLikeUserPostModel
+    {
+        public PostLike PostLikeModel { get; set; }
+        public IEnumerable<Post> PostModel { get; set; }
+        public IEnumerable<User> UserModel { get; set; }
+    }
+}
