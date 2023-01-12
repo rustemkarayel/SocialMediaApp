00cb098123cd106b6c369e3e8a03769bcdf92a29
MHusna <mehmethusnak@gmail.com>
2023-01-12 20:22:14 +0300

    Collection varlığı CreationDate migration eklendi.

---
+++ DataAccessLayer/Migrations/20230112155918_CreationDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreationDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationDate",
+                table: "Collections",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreationDate",
+                table: "Collections");
+        }
+    }
+}
