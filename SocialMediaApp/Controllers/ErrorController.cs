1a5563094dec8c9d0752b2fe2093e4b2223f22f1
rustemkarayel <rustemkarayel07@gmail.com>
2023-01-15 00:04:04 +0300

    Error Page yapıldı.

---
+++ SocialMediaApp/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMediaApp.Controllers
+{
+	[Route("error")]
+	public class ErrorController : Controller
+	{
+		[Route("/Error/HandleError/{code:int}")]
+		public IActionResult HandleError(int code)
+		{
+			ViewBag.code = code.ToString();
+			switch (code)
+			{
+				case 404:
+					ViewBag.msg = "Uh Oh! Page not found!";
+					break;
+				case 500:
+					ViewBag.msg = "Uh Oh! Internal Error!";
+					break;
+			}
+			return View();
+		}
+	}
+}
--- SocialMediaApp/Program.cs
+++ SocialMediaApp/Program.cs
@@ -36,8 +36,8 @@ namespace SocialMediaApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-           
-           
+            app.UseStatusCodePagesWithReExecute("/Error/HandleError/{0}");
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
