c67ce7fa732b178ae84951773cc6171e95894419
Yusufhan-Erbas <erbasyusufhan15@gmail.com>
2023-01-24 12:55:25 +0300

    PostLike sayfalama yapıldı.

--- SocialMediaApp/Controllers/PostLikeController.cs
+++ SocialMediaApp/Controllers/PostLikeController.cs
@@ -1,10 +1,12 @@
 ﻿using BusinessLayer.Concrete;
 using BusinessLayer.Validations;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.Concrete.EntityFramework;
 using EntityLayer;
 using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using SocialMediaApp.Models;
+using SocialMediaApp.PagedList;
 using X.PagedList;
 
 namespace SocialMediaApp.Controllers
@@ -14,10 +16,36 @@ namespace SocialMediaApp.Controllers
         PostLikeManager plm = new PostLikeManager(new EfPostLikeRepository());
         PostManager pm = new PostManager(new EfPostRepository());
         UserManager um = new UserManager(new EfUserRepository());
-        public IActionResult Index(int page=1,int pageSize=10)
+        public IActionResult Index(int page=1,string searchText="")
         {
-            var postlikes = plm.PostLikeList().ToPagedList(page,pageSize);
-            return View(postlikes);
+            //var postlikes = plm.PostLikeList().ToPagedList(page,pageSize);
+            //return View(postlikes);
+
+            int pageSize = 2;
+            Context c = new Context();
+            Pager pager;
+            List<PostLike> data;
+            var itemCounts = 0;
+            if (searchText != "" && searchText != null)
+            {
+                data = c.PostLikes.Where(postLike => postLike.PostLiker.NickName.Contains(searchText) || postLike.Post.PostContent.Contains(searchText)
+              ).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+                itemCounts = c.PostLikes.Where(postLike => postLike.PostLiker.NickName.Contains(searchText) || postLike.Post.PostContent.Contains(searchText)
+              ).ToList().Count;
+            }
+            else
+            {
+                data = c.PostLikes.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+                itemCounts = c.PostLikes.ToList().Count;
+            }
+
+            pager = new Pager(itemCounts, pageSize, page);
+            ViewBag.pager = pager;
+            ViewBag.searchText = searchText;
+            ViewBag.contrName = "PostLike";
+            ViewBag.actionName = "PostLikeList";
+            return View(data);
         }
         [HttpGet]
         public IActionResult Add()
