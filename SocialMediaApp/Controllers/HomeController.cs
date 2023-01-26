d85c533efab178d05e18dd0b432a3d7369eab0f1
rustemkarayel <rustemkarayel07@gmail.com>
2023-01-26 11:41:59 +0300

    UserProfile yapıldı.

--- SocialMediaApp/Controllers/HomeController.cs
+++ SocialMediaApp/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace SocialMediaApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        
+        public IActionResult UserProfile()
+        {
+            return View();
+        }
     }
 }
