4e2372c2f74860d29c920e898beb9fb354c2c8b9
rustemkarayel <rustemkarayel07@gmail.com>
2023-01-26 15:17:09 +0300

    Post kullanıcı arayüzü yapıldı.

--- SocialMediaApp/Controllers/HomeController.cs
+++ SocialMediaApp/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using System.Diagnostics;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer;
 using Microsoft.AspNetCore.Mvc;
 using SocialMediaApp.Models;
 
@@ -6,6 +9,7 @@ namespace SocialMediaApp.Controllers
 {
     public class HomeController : Controller
     {
+        PostManager postManager = new PostManager(new EfPostRepository());
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -19,8 +23,8 @@ namespace SocialMediaApp.Controllers
 		}
 
 		public IActionResult Index()
-        {
-            return View();
+        {           
+            return View(postManager.PostList());
         }
 
         [HttpGet]
@@ -50,7 +54,82 @@ namespace SocialMediaApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        
 
-    }
+		static string GetPrettyDate(DateTime d)
+		{
+			// 1.
+			// Get time span elapsed since the date.
+			TimeSpan s = DateTime.Now.Subtract(d);
+
+			// 2.
+			// Get total number of days elapsed.
+			int dayDiff = (int)s.TotalDays;
+
+			// 3.
+			// Get total number of seconds elapsed.
+			int secDiff = (int)s.TotalSeconds;
+
+			// 4.
+			// Don't allow out of range values.
+			if (dayDiff < 0 || dayDiff >= 31)
+			{
+				return null;
+			}
+
+			// 5.
+			// Handle same-day times.
+			if (dayDiff == 0)
+			{
+				// A.
+				// Less than one minute ago.
+				if (secDiff < 60)
+				{
+					return "just now";
+				}
+				// B.
+				// Less than 2 minutes ago.
+				if (secDiff < 120)
+				{
+					return "1 minute ago";
+				}
+				// C.
+				// Less than one hour ago.
+				if (secDiff < 3600)
+				{
+					return string.Format("{0} minutes ago",
+						Math.Floor((double)secDiff / 60));
+				}
+				// D.
+				// Less than 2 hours ago.
+				if (secDiff < 7200)
+				{
+					return "1 hour ago";
+				}
+				// E.
+				// Less than one day ago.
+				if (secDiff < 86400)
+				{
+					return string.Format("{0} hours ago",
+						Math.Floor((double)secDiff / 3600));
+				}
+			}
+			// 6.
+			// Handle previous days.
+			if (dayDiff == 1)
+			{
+				return "yesterday";
+			}
+			if (dayDiff < 7)
+			{
+				return string.Format("{0} days ago",
+				dayDiff);
+			}
+			if (dayDiff < 31)
+			{
+				return string.Format("{0} weeks ago",
+				Math.Ceiling((double)dayDiff / 7));
+			}
+			return null;
+		}
+	}
 }
