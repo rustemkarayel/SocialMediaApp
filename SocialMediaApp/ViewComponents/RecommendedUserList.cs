aedf7d4c9c11a9f6472a6d05f3cfb28b0c4aa6b5
mhusna <mehmethusnak@gmail.com>
2023-01-27 11:46:44 +0300

    Toplu değişiklik

---
+++ SocialMediaApp/ViewComponents/RecommendedUserList.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMediaApp.ViewComponents
+{
+	public class RecommendedUserList: ViewComponent
+	{
+		
+		public async Task<IViewComponentResult> getRecommendedUserList()
+		{
+			return View();
+		}
+	}
+}
