890b30e74418dffa08299574627506adb883d960
rustemkarayel <rustemkarayel07@gmail.com>
2023-01-28 21:39:54 +0300

    Post için Location null olabilir yapıldı.

---
+++ DataAccessLayer/Migrations/20230128182819_locationNull.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class locationNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Locations_LocationId",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationId",
+                table: "Posts",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Locations_LocationId",
+                table: "Posts",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "LocationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Locations_LocationId",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationId",
+                table: "Posts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Locations_LocationId",
+                table: "Posts",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "LocationId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
